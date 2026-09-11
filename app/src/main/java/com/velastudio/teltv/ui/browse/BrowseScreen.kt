42a1d6eeb7b65a5e54d6ee2de88c36af2483a4d1
manuelmania93-svg <manuelmania93@gmail.com>
2026-09-11 00:41:04 +0000

    Fix browse screen build import

--- app/src/main/java/com/velastudio/teltv/ui/browse/BrowseScreen.kt
+++ app/src/main/java/com/velastudio/teltv/ui/browse/BrowseScreen.kt
@@ -1,5 +1,6 @@
 package com.velastudio.teltv.ui.browse
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
