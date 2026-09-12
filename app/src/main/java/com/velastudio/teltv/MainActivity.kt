dcadb7c1c6c07132067bc88c18ea6c1faef1f74c
manuelmania93-svg <manuelmania93@gmail.com>
2026-09-12 03:36:11 +0000

    Fix layout import for fillMaxWidth in MainActivity

--- app/src/main/java/com/velastudio/teltv/MainActivity.kt
+++ app/src/main/java/com/velastudio/teltv/MainActivity.kt
@@ -5,11 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.*
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
